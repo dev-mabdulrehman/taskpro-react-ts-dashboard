4c294ad27b89c6ad3a61efe196bfa185904b56b3
dev-mabdulrehman <dev.mabdulrehman@gmail.com>
2025-12-09 14:04:38 +0000

    feat(ui): Highlight current active navigation link

--- src/components/BottomNavMobile/BottomNavMobileItem.tsx
+++ src/components/BottomNavMobile/BottomNavMobileItem.tsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router"
+import { Link, useLocation } from "react-router"
 import type { INavLink } from "../../types/navigation"
 
 const BottomNavMobileItem = ({ path, icon, name }: INavLink) => {
+  const location = useLocation();
   return (
-      <Link className="flex flex-col gap-y-1 hover:text-primary items-center text-gray-600" to={path} >
+    <Link className={`flex flex-col gap-y-1 hover:text-primary items-center ${location.pathname == path ? 'text-primary' : 'text-gray-500'}`} to={path} >
           <i className={`${icon} text-2xl`}></i>
           <span className="text-xs">{name}</span>
       </Link>
--- src/components/sidebar/SidebarItem.tsx
+++ src/components/sidebar/SidebarItem.tsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router"
+import { Link, useLocation } from "react-router"
 import type { INavLink } from "../../types/navigation"
 
 const SidebarItem = ({ path, icon, name }: INavLink) => {
+    const location = useLocation();
     return (
-        <Link to={path} className="flex gap-3 items-center w-full cursor-pointer text-white hover:bg-[#2d2c31] p-4 px-8">
+        <Link to={path} className={`flex gap-3 items-center w-full cursor-pointer text-white hover:bg-[#2d2c31] p-4 px-8 ${path === location.pathname ? 'bg-[#2d2c31]' : ''}`}>
             <i className={`${icon} text-2xl`} aria-hidden="true"></i>
             <span className="text-xl font-medium">{name}</span>
         </Link>
